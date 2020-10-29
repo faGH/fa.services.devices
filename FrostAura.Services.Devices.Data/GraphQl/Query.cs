800b847762807b0e585fe102447a0fa8c390ce9e
Dean Martin <dean.martin@frostaura.net>
2020-10-29 08:57:28 +0200

    Removal of specific inefficient queries. Should use WHERE instead.

--- FrostAura.Services.Devices.Data/GraphQl/Query.cs
+++ FrostAura.Services.Devices.Data/GraphQl/Query.cs
@@ -1,13 +1,10 @@
-﻿using FrostAura.Libraries.Core.Extensions.Validation;
-using FrostAura.Services.Devices.Data;
+﻿using FrostAura.Services.Devices.Data;
 using FrostAura.Services.Devices.Data.GraphQl.Types;
 using FrostAura.Services.Devices.Data.Models.Entities;
 using HotChocolate;
 using HotChocolate.Types;
 using HotChocolate.Types.Relay;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace FrostAura.Services.Devices.Shared.Models.GraphQl
 {
@@ -28,26 +25,6 @@ namespace FrostAura.Services.Devices.Shared.Models.GraphQl
         public IQueryable<Device> Devices([Service] DevicesDbContext db) => db.Devices;
 
         /// <summary>
-        /// Device query for GraphQL.
-        /// </summary>
-        /// <param name="db">Database context.</param>
-        /// <param name="id">Unique auto-genenrated id for the device.</param>
-        /// <returns>Collection of queryable devices.</returns>
-        [UseFiltering]
-        [UseSorting]
-        public async Task<Device> Device([Service] DevicesDbContext db, int id)
-        {
-            var device = await db
-                .Devices
-                .Include(d => d.Attributes)
-                .ThenInclude(a => a.Attribute)
-                .FirstOrDefaultAsync(d => d.Id == id);
-
-            return device
-                .ThrowIfNull(nameof(device));
-        }
-
-        /// <summary>
         /// Attributes query for GraphQL.
         /// </summary>
         /// <param name="db">Database context.</param>
@@ -57,24 +34,5 @@ namespace FrostAura.Services.Devices.Shared.Models.GraphQl
         [UseFiltering]
         [UseSorting]
         public IQueryable<Attribute> Attributes([Service] DevicesDbContext db) => db.Attributes;
-
-        /// <summary>
-        /// Attribute query for GraphQL.
-        /// </summary>
-        /// <param name="db">Database context.</param>
-        /// <param name="id">Unique auto-genenrated id for the attribute.</param>
-        /// <returns>Attribute or null.</returns>
-        [UseFiltering]
-        [UseSorting]
-        public async Task<Attribute> Attribute([Service] DevicesDbContext db, int id)
-        {
-            var attribute = await db
-                .Attributes
-                .Include(a => a.DeviceAttributes)
-                .ThenInclude(da => da.Device)
-                .FirstAsync(d => d.Id == id);
-
-            return attribute;
-        }
     }
 }
